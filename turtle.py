7b9e2a4c9e735f0b74da91fc40f635c1612ffe6b
ParkGeonHwi <83908167+ParkGeonHwi@users.noreply.github.com>
2021-11-04 20:39:40 +0900

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,31 @@
+import turtle
+t=turtle.Turtle()
+t.shape("turtle")
+t.width(5)
+
+t.color("yellow")
+t.circle(60)
+
+t.up()
+t.goto(130, 0)
+t.down()
+t.color("green")
+t.circle(60)
+
+t.up()
+t.goto(200, 70)
+t.down()
+t.color("red")
+t.circle(60)
+
+t.up()
+t.goto(70, 70)
+t.down()
+t.color("black")
+t.circle(60)
+
+t.up()
+t.goto(-60, 70)
+t.down()
+t.color("blue")
+t.circle(60)
